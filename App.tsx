6fdbe398f52849da7001e83fb601b55c004e3901
Valery Filipyev <valeranuzhdin1998@mail.ru>
2023-06-19 15:52:02 +0300

    navigate between pages

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { StyleSheet, Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import * as SplashScreen from "expo-splash-screen";
 import * as Font from "expo-font";
+import "react-native-gesture-handler";
+
+import ChatListScreen from "./screens/ChatListScreen";
+import ChatSettingsScreen from "./screens/ChatSettingsScreen";
 
 SplashScreen.preventAutoHideAsync().catch(() => {});
+const Stack = createStackNavigator();
 
 export default function App() {
   const [appIsLoaded, setAppIsLoaded] = useState(false);
@@ -48,11 +55,19 @@ export default function App() {
 
   return (
     <SafeAreaProvider style={styles.container} onLayout={onLayout}>
-      <SafeAreaView>
-        <Text style={styles.label}>
-          Open up App.tsx to start working on your app!
-        </Text>
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={ChatListScreen} />
+          <Stack.Screen
+            name="ChatSettings"
+            component={ChatSettingsScreen}
+            options={{
+              headerTitle: "Settings",
+              headerBackTitle: "Back",
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
@@ -61,8 +76,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
   label: {
     color: "black",
---
+++ screens/ChatListScreen.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { StackNavigationProp } from "@react-navigation/stack";
+
+import { RootStackParamList } from "../types";
+
+interface ChatListScreenProps {
+  navigation: StackNavigationProp<RootStackParamList, "Home">;
+}
+
+const ChatListScreen: React.FC<ChatListScreenProps> = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Chat List Screen</Text>
+
+      <Button
+        title="Go to Settings"
+        onPress={() => {
+          navigation.navigate("ChatSettings");
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default ChatListScreen;
